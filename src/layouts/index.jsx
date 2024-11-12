8ad9dbf12e899fc8048b8e77d589c9b4561d8bf8
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-12 23:52:53 +0900

    (add) mode

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 import Modal from "../components/common/components/Modal";
 import ToastContainer from "../components/common/components/ToastContainer";
+import { useLayoutModeEffect } from "../store/useMode";
 
 export const Layout = () => {
-  // TODO: mode 감지해서 변경
+  useLayoutModeEffect();
+
   return (
     <>
       {/* <Header/>
