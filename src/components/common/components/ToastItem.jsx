f48dd038a490e30261ad682098d1017086f089e5
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-13 22:11:19 +0900

    (update) toast item 내 toast 내용 구체화

--- src/components/common/components/ToastItem.jsx
+++ src/components/common/components/ToastItem.jsx
@@ -1,5 +1,40 @@
-const ToastItem = (toast) => {
-  return <div>{toast.content}</div>;
+import { useEffect } from "react";
+import { omitObject } from "../../../lib/util";
+import useToast from "../../../store/toast/useToast";
+
+const ToastItem = ({ toast }) => {
+  const { remove } = useToast();
+
+  useEffect(() => {
+    let timeout = null;
+
+    if (toast.autoClose) {
+      timeout = setTimeout(() => {
+        remove(toast.id);
+        if (toast.onClose) toast.onClose();
+        clearTimeout(timeout);
+      }, toast.duration);
+    }
+
+    return () => {
+      if (timeout) clearTimeout(timeout);
+    };
+  }, []);
+
+  if (toast.component)
+    return (
+      <toast.component
+        {...omitObject(toast, ["autoClose", "onClose", "duration"])}
+      />
+    );
+
+  return (
+    <div>
+      {toast.showCloseBtn && <button>close</button>}
+      {toast.icon && <div>icon</div>}
+      {toast.message}
+    </div>
+  );
 };
 
 export default ToastItem;
