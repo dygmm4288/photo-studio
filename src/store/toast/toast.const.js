5584959848c52fdcbc7c8e4ad66e420fac4e2905
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-13 21:49:14 +0900

    (add) toast 상수들 파일 추가

---
+++ src/store/toast/toast.const.js
@@ -0,0 +1,16 @@
+export const ToastType = {
+  SUCCESS: "success",
+  ERROR: "error",
+  INFO: "info",
+  WARNING: "warning",
+};
+
+export const ToastPosition = {
+  TOP_RIGHT: "top-right",
+  TOP_LEFT: "top-left",
+  BOTTOM_RIGHT: "bottom-right",
+  BOTTOM_LEFT: "bottom-left",
+  TOP_CENTER: "top-center",
+  BOTTOM_CENTER: "bottom-center",
+  CENTER: "center",
+};
