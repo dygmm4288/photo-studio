1f16b88223596dd6b829c991f6583bdd611d5eba
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-14 23:27:35 +0900

    (add) layout style 추가

---
+++ src/css/layout.styles.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StFlex = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+export const StRow = styled(StFlex)`
+  flex-direction: row;
+`;
+
+export const StColumn = styled(StFlex)`
+  flex-direction: column;
+`;
+
+export const StFlexWrap = styled.div`
+  flex-wrap: wrap;
+`;
