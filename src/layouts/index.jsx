fff59be0e91aab900f72dabbe58cde5c4a4daea4
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-14 23:44:55 +0900

    (update) header layout 적용

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,14 +2,15 @@ import { Outlet } from "react-router";
 import Modal from "../components/common/components/Modal";
 import ToastContainer from "../components/common/components/ToastContainer";
 import { useLayoutModeEffect } from "../store/useMode";
+import Header from "./web/Header";
 
 const Layout = () => {
   useLayoutModeEffect();
 
   return (
     <>
-      {/* <Header/>
-      <Main> */}
+      <Header />
+      {/*<Main> */}
       <Outlet />
       {/* </Main>
       <Footer/> */}
