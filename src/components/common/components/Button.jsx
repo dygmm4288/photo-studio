573d44c2cb8671a36c2f6899d0ba50850c694503
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-14 23:05:40 +0900

    (add) 공통 컴포넌트용 버튼 추가

--- src/components/common/components/Button.jsx
+++ src/components/common/components/Button.jsx
@@ -1,3 +1,9 @@
-const Button = () => {};
+import styled from "styled-components";
+
+const Button = ({ children, ...props }) => {
+  return <StButton {...props}>{children}</StButton>;
+};
+
+const StButton = styled.button``;
 
 export default Button;
