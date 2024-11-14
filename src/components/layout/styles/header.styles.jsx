06044346b4ac2ee87aaa1c058cd426501c0f983a
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-14 23:42:35 +0900

    (style) header layout 스타일 적용

--- src/components/layout/styles/header.styles.jsx
+++ src/components/layout/styles/header.styles.jsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
+import { StFlex } from "../../../css/layout.styles";
 
-export const StHeaderWrapper = styled.div``;
-
-export const StHeaderHeading = styled.h1``;
+export const StHeaderWrapper = styled(StFlex)`
+  justify-content: space-between;
+`;
 
 export const StNavWrapper = styled.nav``;
 
-export const StNavList = styled.ul``;
+export const StNavList = styled(StFlex)`
+  gap: 6.9rem;
+`;
 
 export const StNavItem = styled.li``;
