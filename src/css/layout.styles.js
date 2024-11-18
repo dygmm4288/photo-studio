b08a11cfcc0a9a5641dd9ac734fe1e22d41a7c51
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-18 21:17:20 +0900

    (fix) 객체값 없을 시 undefined 처리

--- src/css/layout.styles.js
+++ src/css/layout.styles.js
@@ -5,13 +5,13 @@ export const flex = () => css`
   flex-wrap: nowrap;
 `;
 
-export const rowBox = ({ gap = 1.4 }) => css`
+export const rowBox = ({ gap = 1.4 } = {}) => css`
   ${flex()}
   flex-direction: row;
   gap: ${gap}rem;
 `;
 
-export const columnBox = ({ gap = 1.4 }) => css`
+export const columnBox = ({ gap = 1.4 } = {}) => css`
   ${flex()}
   flex-direction: column;
   gap: ${gap}rem;
