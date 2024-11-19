4ccc390db1b18e47ce58826174d0cae56bfa6911
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-19 22:56:40 +0900

    (update) footer icon  적용 및 copyright 추가

--- src/components/layout/styles/footer.styles.js
+++ src/components/layout/styles/footer.styles.js
@@ -2,10 +2,12 @@ import styled from "styled-components";
 import {
   bgBlack,
   bgTertiary,
+  caption1,
   heading1,
   heading3,
+  textGray,
 } from "../../../css/fonts.styles";
-import { columnBox, rowBox } from "../../../css/layout.styles";
+import { columnBox, flex, rowBox } from "../../../css/layout.styles";
 
 export const Footer = styled.footer`
   width: 100%;
@@ -23,9 +25,14 @@ export const Head = styled.h1`
 
 export const FooterIconContainer = styled.div`
   ${rowBox({ gap: 1.6 })}
+  align-items: center;
 `;
 
-export const FooterIcon = styled.i``;
+export const FooterIcon = styled.i`
+  ${flex()}
+  align-items:center;
+  justify-content: center;
+`;
 
 export const Line = styled.div`
   width: 2.1rem;
@@ -35,10 +42,23 @@ export const Line = styled.div`
 
 export const FooterInfoContainer = styled.div`
   ${columnBox({ gap: 1.1 })}
+  position: relative;
 `;
 export const FooterInfoWrapper = styled.div`
   ${rowBox({ gap: 1 })}
 `;
 export const FooterText = styled.p`
   ${heading3()}
+`;
+export const FooterCopyrightWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+  ${rowBox({ gap: 0.4 })}
+`;
+
+export const FooterCopyrightText = styled.p`
+  ${caption1()}
+  ${textGray()}
 `;
--- src/layouts/web/Footer.jsx
+++ src/layouts/web/Footer.jsx
@@ -1,23 +1,32 @@
+import CopyrightIcon from "../../assets/icons/CopyrightIcon";
+import FaceBookIcon from "../../assets/icons/FaceBookIcon";
+import InstaIcon from "../../assets/icons/InstaIcon";
+import LocationIcon from "../../assets/icons/LocationIcon";
+import MailIcon from "../../assets/icons/MailIcon";
+import NaverIcon from "../../assets/icons/NaverIcon";
+import TelIcon from "../../assets/icons/TelIcon";
+import TimeIcon from "../../assets/icons/TimeIcon";
+
 import * as St from "../../components/layout/styles/footer.styles";
 const footerInfos = [
   {
     kind: "location",
-    icon: "",
+    icon: LocationIcon,
     content: "제주도 서귀포시 중문로 123",
   },
   {
     kind: "operating_hours",
-    icon: "",
+    icon: TimeIcon,
     content: "운영 시간: 매일 10:00 ~ 18:00",
   },
   {
     kind: "tel",
-    icon: "",
+    icon: TelIcon,
     content: "064-123-4567",
   },
   {
     kind: "email",
-    icon: "",
+    icon: MailIcon,
     content: "forest_pictures_jeju@forestpictures.com",
   },
 ];
@@ -27,19 +36,33 @@ const Footer = () => {
       <St.HeadWrapper>
         <St.Head>포레스트 픽처스</St.Head>
         <St.FooterIconContainer>
-          <St.FooterIcon>인스타</St.FooterIcon>
-          <St.FooterIcon>페이스북</St.FooterIcon>
-          <St.FooterIcon>네이버</St.FooterIcon>
+          <St.FooterIcon>
+            <InstaIcon>인스타</InstaIcon>
+          </St.FooterIcon>
+          <St.FooterIcon>
+            <FaceBookIcon>페이스북</FaceBookIcon>
+          </St.FooterIcon>
+          <St.FooterIcon>
+            <NaverIcon>네이버</NaverIcon>
+          </St.FooterIcon>
         </St.FooterIconContainer>
       </St.HeadWrapper>
       <St.Line />
       <St.FooterInfoContainer>
         {footerInfos.map((info) => (
           <St.FooterInfoWrapper key={`footer-${info.kind}`}>
-            <St.FooterIcon>{info.icon}</St.FooterIcon>
+            <St.FooterIcon>
+              <info.icon />
+            </St.FooterIcon>
             <St.FooterText>{info.content}</St.FooterText>
           </St.FooterInfoWrapper>
         ))}
+        <St.FooterCopyrightWrapper>
+          <CopyrightIcon />
+          <St.FooterCopyrightText>
+            2024 Forest Pictures. All Rights Reserved.
+          </St.FooterCopyrightText>
+        </St.FooterCopyrightWrapper>
       </St.FooterInfoContainer>
     </St.Footer>
   );
