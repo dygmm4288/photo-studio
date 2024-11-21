71b8502b4bf2fc10d0a01ad8cd87db80327ba44e
jihyun-j <jihyun9142@gmail.com>
2024-11-21 12:27:11 +0900

    (feat) 예약 정보 확인 바 하단에 고정하여 추가 완성

--- src/components/booking/styles/BookingStyles.js
+++ src/components/booking/styles/BookingStyles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { body2, heading3 } from "../../../css/fonts.styles";
+import { body2, heading3, label1, textAccent } from "../../../css/fonts.styles";
 import { columnBox } from "../../../css/layout.styles";
 
 export const BookingWrapper = styled.div`
@@ -8,6 +8,8 @@ export const BookingWrapper = styled.div`
   width: 100%;
   margin-top: 10rem;
 `;
+
+// 예약 캘린더 //
 
 // 예약 전 필수 사항 //
 export const ImportantNoteContainer = styled.div`
@@ -36,4 +38,40 @@ export const NotesList = styled.li`
   list-style-position: inside;
   text-indent: 2.4rem;
   ${body2()};
+`;
+
+// 예약자 정보 //
+
+// 예약 옵션 선택 //
+
+// 예약 시간 선택 //
+
+// 예약 확인 바 //
+export const BookingConfirmationBarContainer = styled.div`
+  background-color: #336155;
+  height: 9rem;
+  position: sticky;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BookingInfoWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+  align-items: center;
+  color: #ffffff;
+`;
+
+export const BookingInfo = styled.span`
+  ${textAccent()}
+`;
+
+export const BookingButton = styled.button`
+  background-color: #ff9100;
+  padding: 1rem 3rem;
+  color: #ffffff;
+  ${label1()}
 `;
--- src/components/booking/views/Booking.jsx
+++ src/components/booking/views/Booking.jsx
@@ -1,11 +1,15 @@
 import * as St from "../../booking/styles/BookingStyles";
+import BookingConfirmationBar from "../components/formSection/BookingConfirmationBar";
 // import BookingCalendar from "../components/calendarSection/BookingCalendar";
 import BookingForm from "./BookingForm";
 export default function Booking() {
   return (
-    <St.BookingWrapper>
-      {/* <BookingCalendar /> */}
-      <BookingForm />
-    </St.BookingWrapper>
+    <div>
+      <St.BookingWrapper>
+        {/* <BookingCalendar /> */}
+        <BookingForm />
+      </St.BookingWrapper>
+      <BookingConfirmationBar />
+    </div>
   );
 }
