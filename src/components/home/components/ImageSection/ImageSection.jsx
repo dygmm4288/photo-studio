b688782468b67c200153b1ad22a17d2fade4776c
Rock Kyun <mjnn100801@naver.com>
2024-11-25 22:58:30 +0900

    update(Home): 반응형 디자인 적용

--- src/components/home/components/ImageSection/ImageSection.jsx
+++ src/components/home/components/ImageSection/ImageSection.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import { sectionImages } from "./const";
 import * as St from "../../styles/HomeStyle";
+import useResponsive from "../../hooks/useResponsive";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+import useReviewSection from "../../hooks/useReviewSection";
 
 export default function ImageSection() {
   const [images, setImages] = useState([]);
+  const { isMobile } = useResponsive();
+  const { bgImage, transform, handleSlide, containerRef } =
+    useReviewSection(sectionImages);
 
   useEffect(() => {
     if (images) {
@@ -17,16 +23,41 @@ export default function ImageSection() {
         <St.TypoSub>Our Work</St.TypoSub>
         <St.SectionTitle>포트폴리오 갤러리</St.SectionTitle>
         <St.SectionDescription>
-          지금까지 함께한 소중한 고객들과의 촬영 사진을 통해 포레스트 픽쳐스의
-          감성을 느껴보세요.
+          {isMobile
+            ? `지금까지 함께한 소중한 고객들과의 촬영 사진을 통해\n포레스트 픽쳐스의 감성을 느껴보세요.`
+            : `지금까지 함께한 소중한 고객들과의 촬영 사진을 통해 포레스트 픽쳐스의 감성을 느껴보세요.`}
         </St.SectionDescription>
 
         {/* grid Section */}
-        <St.GridWithUnbalance>
-          {images.map((img, idx) => {
-            return <img src={img} alt="section-image" key={idx} />;
-          })}
-        </St.GridWithUnbalance>
+        {!isMobile && (
+          <St.GridWithUnbalance>
+            {images.map((img, idx) => {
+              return <img src={img} alt="section-image" key={idx} />;
+            })}
+          </St.GridWithUnbalance>
+        )}
+
+        {isMobile && (
+          <St.CarouselWrapper>
+            <St.CarouselButton onClick={() => handleSlide("left")} $left={true}>
+              <FaCaretLeft size={28} />
+            </St.CarouselButton>
+            <St.CarouselButton
+              onClick={() => handleSlide("right")}
+              $right={true}>
+              <FaCaretRight size={28} />
+            </St.CarouselButton>
+            <St.CarouselContainer ref={containerRef} $transform={transform}>
+              {bgImage.map((img) => {
+                return (
+                  <St.CardWrapper $imageSrc={img} key={img}>
+                    <St.ReviewText>{img.review}</St.ReviewText>
+                  </St.CardWrapper>
+                );
+              })}
+            </St.CarouselContainer>
+          </St.CarouselWrapper>
+        )}
       </St.SectionWrapper>
     </div>
   );
--- src/components/home/hooks/useReviewSection.jsx
+++ src/components/home/hooks/useReviewSection.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useRef, useState } from "react";
-import { reviewWithImage } from "../components/reviewSection/const";
+import useResponsive from "./useResponsive";
 
-export default function useReviewSection() {
+export default function useReviewSection(imageList) {
   // 임시 이미지
   const [bgImage, setBgImage] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [cardWidth, setCardWidth] = useState(0);
   const [visibleCard, setVisibleCard] = useState(4);
   const containerRef = useRef(null);
+  const { isMobile } = useResponsive();
 
   useEffect(() => {
     const calculateVisibleCard = () => {
       if (containerRef.current?.children[0]) {
-        setCardWidth(containerRef.current.children[0].offsetWidth + 20);
+        setCardWidth(containerRef.current.children[0].offsetWidth + 10);
 
         // viewport에 보이는 카드 수 계산 (현재 컨테이너 너비 / 카드 너비)
-        setVisibleCard(Math.ceil(containerRef.current.offsetWidth / cardWidth));
+        setVisibleCard(
+          Math.floor(containerRef.current.offsetWidth / cardWidth)
+        );
       }
     };
     calculateVisibleCard();
@@ -26,22 +29,32 @@ export default function useReviewSection() {
   }, [bgImage, cardWidth]);
 
   const handleSlide = (direction) => {
-    setCurrentIndex((prev) => {
-      if (direction === "left") {
-        return prev === bgImage.length - visibleCard ? 0 : prev + 1;
-      } else {
-        return prev === 0 ? bgImage.length - visibleCard : prev - 1;
-      }
-    });
+    if (isMobile) {
+      setCurrentIndex((prev) => {
+        if (direction === "left") {
+          return prev === 0 ? imageList.length - 1 : prev - 1;
+        } else {
+          return prev === imageList.length - 1 ? 0 : prev + 1;
+        }
+      });
+    } else {
+      setCurrentIndex((prev) => {
+        if (direction === "left") {
+          return prev === 0 ? imageList.length - visibleCard : prev - 1;
+        } else {
+          return prev === imageList.length - visibleCard ? 0 : prev + 1;
+        }
+      });
+    }
   };
 
   const transform = -currentIndex * cardWidth;
 
   useEffect(() => {
-    if (bgImage) {
-      setBgImage(reviewWithImage);
+    if (imageList) {
+      setBgImage(imageList);
     }
-  }, [bgImage]);
+  }, [imageList]);
 
   return { bgImage, transform, handleSlide, containerRef };
 }
