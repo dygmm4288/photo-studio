356467bacb29a92cc03b65f260de9ad08c0204d9
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-25 20:03:53 +0900

    (update) footer const 파일 추가 하여 상수 분리

--- src/layouts/web/Footer.jsx
+++ src/layouts/web/Footer.jsx
@@ -1,52 +1,6 @@
 import CopyrightIcon from "../../assets/icons/CopyrightIcon";
-import FaceBookIcon from "../../assets/icons/FaceBookIcon";
-import InstaIcon from "../../assets/icons/InstaIcon";
-import LocationIcon from "../../assets/icons/LocationIcon";
-import MailIcon from "../../assets/icons/MailIcon";
-import NaverIcon from "../../assets/icons/NaverIcon";
-import TelIcon from "../../assets/icons/TelIcon";
-import TimeIcon from "../../assets/icons/TimeIcon";
-
 import * as St from "../../components/layout/styles/footer.styles";
-const footerInfos = [
-  {
-    kind: "location",
-    icon: LocationIcon,
-    content: "제주도 서귀포시 중문로 123",
-  },
-  {
-    kind: "operating_hours",
-    icon: TimeIcon,
-    content: "운영 시간: 매일 10:00 ~ 18:00",
-  },
-  {
-    kind: "tel",
-    icon: TelIcon,
-    content: "064-123-4567",
-  },
-  {
-    kind: "email",
-    icon: MailIcon,
-    content: "forest_pictures_jeju@forestpictures.com",
-  },
-];
-const footerSNS = [
-  {
-    name: "인스타",
-    icon: InstaIcon,
-    href: "https://www.instagram.com",
-  },
-  {
-    name: "페이스북",
-    icon: FaceBookIcon,
-    href: "https://www.facebook.com",
-  },
-  {
-    name: "네이버",
-    icon: NaverIcon,
-    href: "https://naver.com",
-  },
-];
+import { footerInfos, footerSNS } from "./footer.const";
 const Footer = () => {
   return (
     <St.Footer>
