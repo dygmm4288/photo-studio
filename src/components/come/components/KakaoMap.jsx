2b8ae7424c87d83aaeb4b6c1a1cac34c538a1b9b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-11-27 22:39:22 +0900

    (update) kakao map 기능 수정

--- src/components/come/components/KakaoMap.jsx
+++ src/components/come/components/KakaoMap.jsx
@@ -28,13 +28,16 @@ const KakaoMap = () => {
     const map = new window.kakao.maps.Map(container.current, {
       center: new window.kakao.maps.LatLng(33.3617, 126.5292), //지도의 중심좌표.
       level: 9, //지도의 레벨(확대, 축소 정도)
-      disableDoubleClick: false,
-      disableDoubleClickZoom: false,
+      disableDoubleClick: true,
+      disableDoubleClickZoom: true,
       draggable: false,
       keyboardShortcuts: false,
     });
     const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
-    const marker = new window.kakao.maps.Marker({ position: markerPosition });
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosition,
+      clickable: false,
+    });
 
     marker.setMap(map);
   }, [isLoaded, container.current]);
