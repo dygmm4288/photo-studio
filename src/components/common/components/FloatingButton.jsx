87a6ae2ba47319f87e6a08a0fc5dbf0582ae250c
jihyun-j <jihyun9142@gmail.com>
2024-12-04 14:52:32 +0900

    (feat) 예약하기 버튼 클릭 시 예약페이지로 이동

--- src/components/common/components/FloatingButton.jsx
+++ src/components/common/components/FloatingButton.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import CalendarIcon from "../../../assets/icons/CalendarIcon";
 import useMode from "../../../store/useMode";
 import * as St from "../styles/common.styles";
@@ -5,10 +6,16 @@ import * as St from "../styles/common.styles";
 const FloatingButton = () => {
   const { mode } = useMode();
   const isMobile = mode === "mobile";
+  const navigate = useNavigate();
+
   return (
     <St.FloatingButtonWrapper>
       {<CalendarIcon />}
-      {!isMobile && <St.FloatingText>예약하기</St.FloatingText>}
+      {!isMobile && (
+        <St.FloatingText onClick={() => navigate("/booking")}>
+          예약하기
+        </St.FloatingText>
+      )}
     </St.FloatingButtonWrapper>
   );
 };
