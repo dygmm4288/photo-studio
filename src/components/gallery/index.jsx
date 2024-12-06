787b3532a6bbdd02fccdebefd2877892bb3ae23e
Rock Kyun <mjnn100801@naver.com>
2024-12-06 20:35:11 +0900

    add(gallery): 갤러리 페이지 추가 /gallery

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -3,14 +3,40 @@ import { reviewWithImage } from "../home/components/reviewSection/const";
 import * as CarouselSt from "../home/styles/HomeStyle";
 import * as St from "./styles/galleryStyle";
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+import { useState } from "react";
+
+const TABS = [
+  { name: "제주감성", value: "jeju" },
+  { name: "가족", value: "family" },
+  { name: "웨딩", value: "wedding" },
+  { name: "여행", value: "trip" },
+  { name: "커플", value: "couple" },
+];
 
 export default function Gallery() {
   const { bgImage, containerRef, handleSlide, transform } =
     useCarousel(reviewWithImage);
+  const [tab, setTab] = useState("jeju");
+
+  const handleTab = (tabValue) => {
+    setTab(tabValue);
+  };
 
   return (
     <St.GalleryWrapper>
-      <St.CarouselWrapper>
+      <St.GalleryTitle>갤러리</St.GalleryTitle>
+      <St.TabWrapper>
+        {TABS.map((menu) => (
+          <St.Tab
+            onClick={() => handleTab(menu.value)}
+            $isSelected={tab === menu.value}
+            key={menu.name}>
+            {menu.name}
+          </St.Tab>
+        ))}
+      </St.TabWrapper>
+      <St.SubTitle>제주 감성 스냅 촬영 고객의 후기</St.SubTitle>
+      <St.CarouselWrapper style={{ marginBottom: "8.3rem" }}>
         <CarouselSt.CarouselButton
           $left={true}
           onClick={() => handleSlide("left")}>
@@ -31,6 +57,20 @@ export default function Gallery() {
           })}
         </CarouselSt.CarouselContainer>
       </St.CarouselWrapper>
+      <St.SubTitle>제주 감성 스냅 촬영 사진</St.SubTitle>
+      <St.GridWrapper>
+        {reviewWithImage.map((img) => {
+          return (
+            <img
+              src={img.img}
+              alt="section-image"
+              key={img.img}
+              width={475}
+              height={485}
+            />
+          );
+        })}
+      </St.GridWrapper>
     </St.GalleryWrapper>
   );
 }
--- src/components/gallery/styles/galleryStyle.js
+++ src/components/gallery/styles/galleryStyle.js
@@ -1,10 +1,20 @@
 import styled from "styled-components";
+import { heading1, heading2 } from "../../../css/fonts.styles";
 
 export const GalleryWrapper = styled.div`
   width: 1000%;
   min-height: calc(100vh);
   max-width: 144rem;
   margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  padding: 9rem 0;
+`
+export const GalleryTitle = styled.p`
+  ${heading1}
+  margin-bottom: 5.6rem;
 `
 
 export const CarouselWrapper = styled.div`
@@ -12,4 +22,55 @@ export const CarouselWrapper = styled.div`
   max-width: 100%;
   overflow: hidden;
   position: relative;
+`
+
+export const TabWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  color: var(--primary);
+  margin-bottom: 5.6rem;
+`
+
+export const Tab = styled.p`
+  ${heading2()}
+  position: relative;
+  cursor: pointer;
+  font-weight: ${props => props.$isSelected ? 600 : 500};
+
+
+  &::after {
+    content: '';
+    width: ${props => props.$isSelected ? '100%' : '0%'};
+    height: .3rem;
+    background-color: var(--primary);
+    position: absolute;
+    left: 0;
+    bottom: -.4rem;
+    transition: all .25s ease;
+  }
+
+  &:hover {
+    &::after {
+      width: 100%;
+    }
+  }
+`
+
+export const SubTitle = styled.p`
+  ${heading2({ bold: true })}
+  color: var(--primary);
+  margin-right: auto;
+  margin-block: 1.4rem;
+`
+
+
+export const GridWrapper = styled.div`
+  width: 100%;
+  max-width: 144rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
 `
