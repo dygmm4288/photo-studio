2d42e174a49fa148857566da6f8e2e00c91a3634
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-12-16 22:28:31 +0900

    (update) home revert

--- src/components/home/views/Home.jsx
+++ src/components/home/views/Home.jsx
@@ -1,5 +1,3 @@
-import supabase from "../../../../supabase_login/src/main";
-import Input from "../../common/components/Input";
 import * as St from "../../home/styles/HomeStyle";
 import ImageSection from "../components/ImageSection/ImageSection";
 import ReviewSection from "../components/reviewSection/ReviewSection";
@@ -7,17 +5,8 @@ import MainSection from "./MainSection";
 import ServiceSection from "./ServiceSection";
 
 export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const onChange = () => {
-    const api = {email: setEmail, password: setPassword};
-  }
-
-
   return (
     <St.HomeWrapper>
-      <Input type='email' label="email" value={email} />
       <MainSection />
       <ServiceSection />
       <ImageSection />
