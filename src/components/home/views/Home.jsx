5580d8e60296d0883d5fa1b04893217325ebdf0d
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-12-16 22:24:21 +0900

    init sub module

--- src/components/home/views/Home.jsx
+++ src/components/home/views/Home.jsx
@@ -1,4 +1,5 @@
 import supabase from "../../../../supabase_login/src/main";
+import Input from "../../common/components/Input";
 import * as St from "../../home/styles/HomeStyle";
 import ImageSection from "../components/ImageSection/ImageSection";
 import ReviewSection from "../components/reviewSection/ReviewSection";
@@ -6,9 +7,17 @@ import MainSection from "./MainSection";
 import ServiceSection from "./ServiceSection";
 
 export default function Home() {
-  console.log("supabase auth", supabase.auth);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = () => {
+    const api = {email: setEmail, password: setPassword};
+  }
+
+
   return (
     <St.HomeWrapper>
+      <Input type='email' label="email" value={email} />
       <MainSection />
       <ServiceSection />
       <ImageSection />
